9c96434a67832e8036b173ddb9f52a272ee2b327
phuc-kun <www.narutocntt@gmail.com>
2018-05-16 10:49:36 +0700

    modified getFilmsById function

--- server/controllers/films.js
+++ server/controllers/films.js
@@ -1,24 +1,12 @@
 var filmsModel = require('../models/film.js');
 exports.getFilmById = function(req,res){
 	var flag = 0;
-	filmsModel.find()
+	filmsModel.findById(req.params.id)
 	.then(doc => {
-		for(var i = 0; i < doc.length; i++)
-		{
-			if(doc[i].id == req.params.id)
-			{
-				res.json(doc[i]);
-				flag = 1;
-				break;
-			}
-		}
-		if(flag == 0)
-		{
-			res.status(500).send('incorrect Id')
-		}
+		res.status(200).json(doc)
 	})
 	.catch(err => {
-		res.status(500).send('error load films from database')
+		res.status(500).send('incorrect Id')
 	})
 }
 exports.getAllFilms = function(req,res){
